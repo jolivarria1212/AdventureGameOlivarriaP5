991af1e907f3e43a08c99b5dadc722e4945e6c49
Jose Olivarria <145067078+jolivarria1212@users.noreply.github.com>
2023-11-08 13:47:56 -0800

    I added colliders
    I make Ruby stop jittering and added a collider to Ruby and The Boxes

--- RubysAdventureP5/Assets/Scripts/RubyController.cs
+++ RubysAdventureP5/Assets/Scripts/RubyController.cs
@@ -1,26 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
-    float horzontal;
+    float horizontal;
     float vertical;
     // Start is called before the first frame update
     void Start()
     {
-       
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+       
+        rigidbody2d.MovePosition(position);
     }
-}
+}   
