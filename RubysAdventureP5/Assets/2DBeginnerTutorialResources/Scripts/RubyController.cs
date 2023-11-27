e9138b3d895b58711cdeb3e099847df49e88a4f3
Jose Olivarria <145067078+jolivarria1212@users.noreply.github.com>
2023-11-27 12:19:52 -0800

    I added animationns to ruby
    I made ruby annimate when she hits somethinng.

--- RubysAdventureP5/Assets/2DBeginnerTutorialResources/Scripts/RubyController.cs
+++ RubysAdventureP5/Assets/2DBeginnerTutorialResources/Scripts/RubyController.cs
@@ -20,11 +20,15 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2 (1,0);
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,6 +37,10 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
